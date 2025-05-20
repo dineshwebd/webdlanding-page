c5b4b3a7715836a33b14d9aa568012a784c1e413
dineshwebd <dxsn.webd@gmail.com>
2025-05-20 15:08:51 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+  const swiper = new Swiper(".eventSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
